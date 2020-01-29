5669151dd1136a198bea59a52f30adc0875a112a
Pietro <pietropugliesi@icloud.com>
2020-01-29 15:59:00 -0300

    arrumado problema do label se tablw vazia
    falta arrumar o escalamento do dynamic tedxt

--- projetoRestIOS/Controller/NotasViewController.swift
+++ projetoRestIOS/Controller/NotasViewController.swift
@@ -14,20 +14,36 @@ class NotasViewController: UIViewController {
 	
 	let apiHandler = APIHandler()
 	
-	var jsonObjects:[JsonObject]?{
+	var emptyTableViewLabel = UILabel()
+	
+	var notes:[Attributes] = [] //note list (json title, content and date attributes only)
+	
+	var activityIndicator = UIActivityIndicatorView()
+	
+	var jsonObjects:[JsonObject] = [] {//json objects stored in database
 		didSet{
-			if jsonObjects?.count == 0{//hides table view if empty
-				tableView.isHidden = true
+			
+			//updates note list (json title, content and date attributes only)
+			self.notes = jsonObjects.map{$0.attributes}
+
+			//hides table view if empty
+			if jsonObjects.count == 0{
+				//view updates must be done in main thread
+				OperationQueue.main.addOperation {
+					self.tableView.isHidden = true
+					self.emptyTableViewLabel.isHidden = false
+				}
+				
 			}else{
-				tableView.isHidden = false
+				OperationQueue.main.addOperation {
+					self.emptyTableViewLabel.isHidden = true
+					self.tableView.isHidden = false
+				}
 			}
 		}
 	}
 	
-	var notes:[Attributes]?
-	
-	var activityIndicator = UIActivityIndicatorView()
-	
+
 	fileprivate func configureActivityIndicator() {
 		activityIndicator.hidesWhenStopped = true
 		activityIndicator.style = .large
@@ -47,9 +63,21 @@ class NotasViewController: UIViewController {
 		tableView.refreshControl?.endRefreshing()
 	}
 	
+	fileprivate func configureEmptyTableViewLabel() {
+		emptyTableViewLabel.frame = self.view.frame
+		emptyTableViewLabel.adjustsFontForContentSizeCategory = true
+		emptyTableViewLabel.numberOfLines = 0
+		emptyTableViewLabel.text = "Você ainda não tem nenhuma nota..."
+		emptyTableViewLabel.textAlignment = .center
+		emptyTableViewLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: (emptyTableViewLabel.font)!)
+		self.view.addSubview(emptyTableViewLabel)
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tableView.isHidden = true
+		
+		configureEmptyTableViewLabel()
 		
 		//activity indicator and table view refresh control
 		configureActivityIndicator()
@@ -60,28 +88,22 @@ class NotasViewController: UIViewController {
 	fileprivate func updateTableView() {
 		apiHandler.getAllNotes { (jsons) in
 			self.jsonObjects = jsons
-			self.notes = jsons.map{$0.attributes}
-
+			
 			DispatchQueue.main.async {
 				self.tableView.reloadData()
 			}
 		}
 	}
 	
-	//animates activity indicator and remove it when table view loaded
-	override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-		activityIndicator.startAnimating()
+	//load or unwind here or back
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
 		
 		updateTableView()
 	}
 	
-	//animates activity indicator and remove it when table view loaded
-	@IBAction func unwindToViewNotes(_ unwindSegue: UIStoryboardSegue) {
-		activityIndicator.startAnimating()
-		
-		updateTableView()
-	}
+	@IBAction func unwindToViewNotes(_ unwindSegue: UIStoryboardSegue) {}
+	
 }
 
 
@@ -89,14 +111,14 @@ extension NotasViewController: UITableViewDelegate, UITableViewDataSource{
 	
 	//number of rows
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return notes?.count ?? 0
+		return jsonObjects.count
 	}
 	
 	//cell for row at
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath) as! NotasTableViewCell
 		
-		let note = notes![indexPath.row]
+		let note = notes[indexPath.row]
 		cell.titleLabel.text = note.title
 		cell.dateLabel.text = apiHandler.formatDate(note)
 		
@@ -108,17 +130,18 @@ extension NotasViewController: UITableViewDelegate, UITableViewDataSource{
 	//swipe editing stylw. Automatic for left to right languages
 	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
 		if editingStyle == .delete{
-			apiHandler.deleteNote(id: jsonObjects![indexPath.row].id)
-			notes?.remove(at: indexPath.row)
+			apiHandler.deleteNote(id: jsonObjects[indexPath.row].id)
+			jsonObjects.remove(at: indexPath.row)
 			tableView.deleteRows(at: [indexPath], with: .automatic)
-			updateTableView()
 			
+			//reload data
+			updateTableView()
 		}
 	}
 	
 	//select cell
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		self.performSegue(withIdentifier: "toViewNote", sender: notes![indexPath.row])
+		self.performSegue(withIdentifier: "toViewNote", sender: notes[indexPath.row])
 	}
 	
 	//loads cell view when selected
